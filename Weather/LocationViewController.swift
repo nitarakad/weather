2d451bf80f17433e993280da14281a61bb91a8cc
Nitya Tarakad <nitarakad@gmail.com>
2019-11-07 16:06:43 -0500

    made it into 2 VCs instead

---
+++ Weather/LocationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LocationViewController.swift
+//  Weather
+//
+//  Created by Nitya Tarakad on 11/7/19.
+//  Copyright © 2019 Nitya Tarakad. All rights reserved.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+    
+    let locationTitle = "Set Your Location"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemBlue
+    }
+}
