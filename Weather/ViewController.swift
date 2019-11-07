21d108e078f0125f416918a0f419ede7238d5d22
Nitya Tarakad <nitarakad@gmail.com>
2019-11-06 19:07:02 -0500

    made button to click on to get to location choice screen

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,11 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var welcomeButton: UIButton!
+    var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        welcomeButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        welcomeButton.backgroundColor = .cyan
+        welcomeButton.setTitle("Welcome", for: .normal)
+        welcomeButton.addTarget(self, action: #selector(navLocationScreen), for: .touchUpInside)
+        welcomeButton.center = self.view.center
+        self.view.addSubview(welcomeButton)
     }
+    
+    @objc func navLocationScreen() {
+        print("clicked")
+        welcomeButton.removeFromSuperview()
+        myLabel = UILabel(frame: CGRect(x: 100, y: 400, width: 200, height: 100))
+        myLabel.textColor = .blue
+        myLabel.text = "location stuff"
+        self.view.addSubview(myLabel)
+        
+    }
+    
+    
 
 
 }
