ff22917b3bf4df6056d9a61c33ccfdf3edc7a87e
Nitya Tarakad <nitarakad@gmail.com>
2019-12-19 14:42:19 -0800

    cache file that might be useful

---
+++ Weather/ClothingCache.swift
@@ -0,0 +1,71 @@
+//
+//  ClothingCache.swift
+//  Weather
+//
+//  Created by Nitya Tarakad on 12/19/19.
+//  Copyright © 2019 Nitya Tarakad. All rights reserved.
+//
+
+import Foundation
+
+final class ClothingCache<Key: Hashable, Value> {
+    private let wrapped = NSCache<WrappedKey, Entry>()
+    
+    func insert(_ value: Value, forKey key: Key) {
+        let entry = Entry(value: value)
+        wrapped.setObject(entry, forKey: WrappedKey(key))
+    }
+
+    func value(forKey key: Key) -> Value? {
+        let entry = wrapped.object(forKey: WrappedKey(key))
+        return entry?.value
+    }
+
+    func removeValue(forKey key: Key) {
+        wrapped.removeObject(forKey: WrappedKey(key))
+    }
+}
+
+private extension ClothingCache {
+    final class WrappedKey: NSObject {
+        let key: Key
+        
+        init(_ key: Key) { self.key = key }
+
+        override var hash: Int { return key.hashValue }
+
+        override func isEqual(_ object: Any?) -> Bool {
+            guard let value = object as? WrappedKey else {
+                return false
+            }
+
+            return value.key == key
+        }
+    }
+}
+
+private extension ClothingCache {
+    final class Entry {
+        let value: Value
+
+        init(value: Value) {
+            self.value = value
+        }
+    }
+}
+
+extension ClothingCache {
+    subscript(key: Key) -> Value? {
+        get { return value(forKey: key) }
+        set {
+            guard let value = newValue else {
+                // If nil was assigned using our subscript,
+                // then we remove any value for that key:
+                removeValue(forKey: key)
+                return
+            }
+
+            insert(value, forKey: key)
+        }
+    }
+}
