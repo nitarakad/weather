a71ab197f92b3fb3e98135bbea0fdb8506d714ec
Nitya Tarakad <nitarakad@gmail.com>
2019-12-29 00:20:10 -0800

    added file to get weather info from Dark Sky API

---
+++ Weather/DarkSkyWeatherInfo.swift
@@ -0,0 +1,13 @@
+//
+//  DarkSkyWeatherInfo.swift
+//  Weather
+//
+//  Created by Nitya Tarakad on 12/29/19.
+//  Copyright © 2019 Nitya Tarakad. All rights reserved.
+//
+
+import Foundation
+
+class DarkSkyWeatherInfo {
+    
+}
