01d13c6932697ceec0b831d390f17e75dcc48493
Nitya Tarakad <nitarakad@gmail.com>
2019-12-29 00:19:01 -0800

    obtained weather stats from OpenWeatherMap

--- Weather/WeatherInfo.swift
+++ Weather/WeatherInfo.swift
@@ -13,9 +13,9 @@ class WeatherInfo {
     // private let openWeatherMapBaseURL = "https://api.openweathermap.org/data/2.5/weather" // 1 day
     private let openWeatherMapBaseURL = "https://api.openweathermap.org/data/2.5/forecast" // 5 day
     private let openWeatherMapAPIKey = "c89bc1dcb1b2def2d0944f132f4bf16a"
+    private var weatherListInfoCount = 0
     
     func getWeather(city: String) {
-        
         let networkingSession = URLSession.shared
         let URLString = "\(openWeatherMapBaseURL)?APPID=\(openWeatherMapAPIKey)&q=\(city)"
         let weatherRequestURL = URL(string: URLString)!
@@ -25,7 +25,7 @@ class WeatherInfo {
                 print("Error obtained: \(error)")
             } else {
                 let stringToPrint = String(data: data!, encoding: String.Encoding.utf8)
-                print("Data:\n\(stringToPrint!)")
+                //print("Data:\n\(stringToPrint!)")
                 do {
                     let weatherInfo = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: AnyObject]
                     /*
@@ -38,19 +38,68 @@ class WeatherInfo {
                      *
                      */
                     
-                    let city = weatherInfo["city"]!["name"]!
-                    print(city!)
-                    
-                    let numberOfItems = weatherInfo["cnt"]! as! Int
-                    print(numberOfItems)
-                    
-                    let theList = weatherInfo["list"]! as! [[String: AnyObject]]
-                    
-                    for i in 0..<numberOfItems {
-                        print("date: \(theList[i]["dt_txt"]!)")
-                        print("max temp: \(theList[i]["main"]!["temp_max"]!)")
-                        print("min temp: \(theList[i]["main"]!["temp_min"]!)")
+                    let weatherInfoList = weatherInfo["list"]! as! [[String: AnyObject]]
+                    let currDateAndTimeRaw = weatherInfoList[0]["dt_txt"]! as! String
+                    var currDateAndTime = currDateAndTimeRaw.split(separator: " ").map { (substring) in
+                        return String(substring)
                     }
+                    
+                    print("date starts at: \(currDateAndTime[0])")
+                    print("time starts at: \(currDateAndTime[1])")
+                    print(weatherInfoList)
+                    self.weatherListInfoCount = weatherInfo["cnt"] as! Int
+                    var allDates = Array<String>()
+                    var allMaxTemps = Array<Array<Double>>()
+                    var allMinTemps = Array<Array<Double>>()
+                    allDates.append(currDateAndTime[0])
+                    allMaxTemps.append(Array<Double>())
+                    allMinTemps.append(Array<Double>())
+                    var currDay = 0
+                    for i in 0..<self.weatherListInfoCount {
+                        let dateAndTimeRaw = weatherInfoList[i]["dt_txt"]! as! String
+                        //print("***DATE AND TIME RAW: \(dateAndTimeRaw)")
+                        let dateAndTime = dateAndTimeRaw.split(separator: " ").map { (substring) in
+                            return String(substring)
+                        }
+                        
+                        //print("***DATE: \(dateAndTime)")
+                        if currDateAndTime[0] == dateAndTime[0] {
+                            if (currDay < 5) {
+                                //print("***CURR DATE: \(currDateAndTime)")
+                                //print("***DATE: \(dateAndTime)")
+                                let maxTemp = weatherInfoList[i]["main"]!["temp_max"]! as! Double
+                                let minTemp = weatherInfoList[i]["main"]!["temp_min"]! as! Double
+                                allMaxTemps[currDay].append(maxTemp)
+                                allMinTemps[currDay].append(minTemp)
+                            }
+                        } else {
+                            currDay = currDay + 1
+                            if (currDay < 5) {
+                                //print("Date: \(currDateAndTime)\nmaxTemps:\(allMaxTemps[currDay-1])\nminTemps:\(allMinTemps[currDay-1])")
+                                allMaxTemps.append(Array<Double>())
+                                allMinTemps.append(Array<Double>())
+                                currDateAndTime = dateAndTime
+                                allDates.append(currDateAndTime[0])
+                                let maxTemp = weatherInfoList[i]["main"]!["temp_max"]! as! Double
+                                let minTemp = weatherInfoList[i]["main"]!["temp_min"]! as! Double
+                                allMaxTemps[currDay].append(maxTemp)
+                                allMinTemps[currDay].append(minTemp)
+                            }
+                        }
+                    }
+                    
+                    var maxTempForDate = Dictionary<String, Double>()
+                    var minTempForDate = Dictionary<String, Double>()
+                    
+                    var i = 0
+                    for date in allDates {
+                        maxTempForDate[date] = self.maxTempOf(allMaxTemps[i])
+                        minTempForDate[date] = self.minTempOf(allMinTemps[i])
+                        i += 1
+                    }
+                    
+//                    print("max: \(maxTempForDate)")
+//                    print("min: \(minTempForDate)")
                     
                 } catch let jsonError as Error {
                     print("JSON error description: \(jsonError)")
@@ -61,5 +110,29 @@ class WeatherInfo {
         task.resume()
         
     }
+    
+    //TODO: write method that gets the max temp out of the 8 max temps given in a day
+    func maxTempOf(_ maxTempArray: [Double]) -> Double {
+        var maxTemp = maxTempArray[0]
+        for temp in maxTempArray {
+            if temp > maxTemp {
+                maxTemp = temp
+            }
+        }
+        return maxTemp
+    }
+    
+    //TODO: write method that gets the min temp out of the 8 min temps given in a day
+    func minTempOf(_ minTempArray:[Double]) -> Double {
+        var minTemp = minTempArray[0]
+        for temp in minTempArray {
+            if temp < minTemp {
+                minTemp = temp
+            }
+        }
+        return minTemp
+    }
+    
+    
     
 }
