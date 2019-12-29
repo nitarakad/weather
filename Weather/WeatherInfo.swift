46779a1994bd51dd233e3044d107babf206d8150
Nitya Tarakad <nitarakad@gmail.com>
2019-12-28 19:42:40 -0800

    get all weather data for 5 days; prints out max and min temp every 3 hours/5 days

--- Weather/WeatherInfo.swift
+++ Weather/WeatherInfo.swift
@@ -10,7 +10,8 @@ import Foundation
 
 class WeatherInfo {
     
-    private let openWeatherMapBaseURL = "https://api.openweathermap.org/data/2.5/weather"
+    // private let openWeatherMapBaseURL = "https://api.openweathermap.org/data/2.5/weather" // 1 day
+    private let openWeatherMapBaseURL = "https://api.openweathermap.org/data/2.5/forecast" // 5 day
     private let openWeatherMapAPIKey = "c89bc1dcb1b2def2d0944f132f4bf16a"
     
     func getWeather(city: String) {
@@ -25,6 +26,35 @@ class WeatherInfo {
             } else {
                 let stringToPrint = String(data: data!, encoding: String.Encoding.utf8)
                 print("Data:\n\(stringToPrint!)")
+                do {
+                    let weatherInfo = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: AnyObject]
+                    /*
+                     ALL KEYS:
+                     *city: coord (lat, lon), country, id, name, population, sunrise, sunset, timezone
+                     *list: clouds (all), dt, "dt_txt", main ("feels_like", "grnd_level", humidity,
+                     pressure, "sea_level", temp, "temp_kf", "temp_max", "temp_min"), sys (pod),
+                     weather (description, icon, id, main), wind (deg, speed)
+                        - there are 40 occurences of each of these in the list
+                     *
+                     */
+                    
+                    let city = weatherInfo["city"]!["name"]!
+                    print(city!)
+                    
+                    let numberOfItems = weatherInfo["cnt"]! as! Int
+                    print(numberOfItems)
+                    
+                    let theList = weatherInfo["list"]! as! [[String: AnyObject]]
+                    
+                    for i in 0..<numberOfItems {
+                        print("date: \(theList[i]["dt_txt"]!)")
+                        print("max temp: \(theList[i]["main"]!["temp_max"]!)")
+                        print("min temp: \(theList[i]["main"]!["temp_min"]!)")
+                    }
+                    
+                } catch let jsonError as Error {
+                    print("JSON error description: \(jsonError)")
+                }
             }
         }
         
